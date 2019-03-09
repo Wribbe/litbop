81e0148a5cbb1c854169d7a6ccf92cfe75bb6b6c
Stefan Eng <eng_steff@hotmail.com>
2019-03-09 15:07:14 +0100

    A more compact solution that supports the >>+ syntax.

--- README.py
+++ README.py
@@ -1,20 +1,53 @@
 """
 This is some text that should not be parsed as code.
 
-<<hello world.py>>=
+<<say hello world>>=
+print("Hello World")
+@
+
+Try to add to the hello world section by using the `>>+` syntax.
+
+<<say hello world>>+
+print("Addition to hello world.")
+@
+
+These should all add up in the end, ensure they do.
+
+<<say hello world>>+
+print("Second addition to hello world.")
+@
+
+Define the file that uses the say hello world segment.
+
+<<hello_world.py>>=
 def main():
+  # Comment that should stay in palace.
   <<say hello world>>
 if __name__ == "__main__":
   main()
 @
 
-<<say hello world>>=
-print("Hello World")
-@
 """
-ls = zip(*[' '*i+open(__file__).read()+' '*(2-i) for i in range(3)])
-d = [i for i,(c1,c2,c3) in enumerate(ls) if c1+c2=="<<" or c2+c3==">>"]
-print(d)
-#print([(c1,c2,c3) for zip(
-
-#print([i if c == "=" else 0 for i,c in enumerate(open(__file__).read())])
+import re, os
+ms=[m.split('\n',1) for m in re.findall(r"<<.*?@",open(__file__).read(),re.S)[:-1]]
+md={k[:-1]:'' for k,v in ms}
+for k,v in ms:
+  md[k[:-1]] += v[:-1]
+h_prev=None
+while True:
+  h_prev=hash(str(md))
+  for k,v in md.items():
+    for pat,sub in md.items():
+      ind = [m.strip('\n') for m in re.findall(rf"\s+{pat}",v)]
+      if ind:
+        ind = len(ind[0]) - len(ind[0].lstrip())
+        ls = sub.splitlines()
+        sub = os.linesep.join(ls[:1] + [' '*ind+l.strip() for l in ls[1:]])
+      md[k] = re.sub(pat,sub,md[k])
+  if h_prev == hash(str(md)):
+    break
+if not os.path.isdir("out"):
+  os.makedirs("out")
+for k,v in md.items():
+  if '.' in k:
+    open(os.path.join("out", k[2:-2]), 'w').write(v)
