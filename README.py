8450f66882080d9f070f4642274baae58cca2478
Stefan Eng <eng_steff@hotmail.com>
2019-03-09 12:55:04 +0100

    Getting indexer for "<<" and ">>".

--- README.py
+++ README.py
@@ -12,50 +12,9 @@ if __name__ == "__main__":
 print("Hello World")
 @
 """
+ls = zip(*[' '*i+open(__file__).read()+' '*(2-i) for i in range(3)])
+d = [i for i,(c1,c2,c3) in enumerate(ls) if c1+c2=="<<" or c2+c3==">>"]
+print(d)
+#print([(c1,c2,c3) for zip(
 
-import os
-
-tags = {}
-lines = [line.rstrip() for line in open(__file__).readlines()]
-
-DIR_OUT = "out"
-
-def_tag = None
-for line in lines:
-  line_stripped = line.strip()
-  if line_stripped.startswith("<<"):
-    if line_stripped.endswith("="):
-      def_tag = line_stripped
-  if def_tag:
-    lines_in_def = tags.get(def_tag)
-    if not lines_in_def:
-      lines_in_def = tags[def_tag] = []
-    lines_in_def.append(line)
-  if line_stripped == "@":
-    def_tag = None
-
-while True:
-  hash_tags_prev = hash(str(tags))
-  for tag, lines in tags.items():
-    new_lines = []
-    for line in lines:
-      line_stripped = line.strip()
-      if line_stripped.startswith("<<") and line_stripped.endswith(">>"):
-        ind = ' '*(len(line) - len(line.lstrip()))
-        lines_in_tag = [f"{ind}{l}" for l in tags[f"{line_stripped}="][1:-1]]
-        new_lines += lines_in_tag
-      else:
-        new_lines.append(line)
-    tags[tag] = new_lines
-  hash_tags_current = hash(str(tags))
-
-  if hash_tags_current == hash_tags_prev:
-    break
-
-if not os.path.isdir(DIR_OUT):
-  os.makedirs(DIR_OUT)
-for key, lines in tags.items():
-  if '.' in key and '=' in key:
-    filename = key[2:-3]
-    with open(os.path.join(DIR_OUT, filename), 'w') as fh:
-      fh.write(os.linesep.join(lines[1:-1]))
+#print([i if c == "=" else 0 for i,c in enumerate(open(__file__).read())])
